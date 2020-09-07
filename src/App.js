5e1ac8fd42864c9515d4160c032d98c8af07e0f9
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-07 23:06:39 +0100

    populate select element eith countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { 
+          FormControl,
+          MenuItem,
+          Select
+          } from '@material-ui/core'
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("global");
+  
+  useEffect(() => {
+const getCountries = async () => {
+ const response = await (await fetch('https://disease.sh/v3/covid-19/countries'))
+ const res = await response.json()
+ const countries = res.map((country) => ({
+   name: country.country,
+   value: country.countryInfo.iso2
+  }));
+ setCountries(countries);
+}
+getCountries()
+  }, [])
+
+  const onCountryChange = async (e) => {
+     let countryCode = e.target.value
+     setCountry(countryCode)
+  }
   return (
     <div className="app">
-      <h1> Covid 19 tracker</h1>
+      <div className="app__header  ">
+        <h1> Covid 19 Tracker</h1>
+        <FormControl className="app__dropdown">
+        <Select 
+          variant="outlined"
+          value={country}
+          onChange={onCountryChange}
+        >
+          <MenuItem value="global">Global</MenuItem>
+          {
+            countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+        </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
